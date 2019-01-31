44acfdecb258b6caeb0c474b1475f1986594d419
mrlewismharris <lewis_h_97@hotmail.co.uk>
2019-01-31 20:01:51 +0000

    Update conv24kwebm.bat

--- conv24kwebm.bat
+++ conv24kwebm.bat
@@ -1,9 +1,11 @@
 @ECHO off
+set bitrate=32768
+
 mediainfo --output=General;%%Duration%%\r\n %1 > templength.txt
 set /p VV=<templength.txt
 del /f templength.txt
 
-set /A b=(32768/(%VV%/1000+1))-100
+set /A b=(%bitrate%/(%VV%/1000+1))-100
 if %VV% gtr 20000 (
 	ffmpeg -i %1 -vf scale=960:540 -c:v libvpx -b:v %b%K -maxrate %b%K -bufsize 10K -c:a libvorbis -b:a 64K %1.webm
 ) else (
