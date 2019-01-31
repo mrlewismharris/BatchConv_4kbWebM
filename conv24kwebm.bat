61575243c93079726826f401cc83f3319add0366
mrlewismharris <lewis_h_97@hotmail.co.uk>
2019-01-31 19:42:58 +0000

    Add files via upload

---
+++ conv24kwebm.bat
@@ -0,0 +1,12 @@
+@ECHO off
+mediainfo --output=General;%%Duration%%\r\n %1 > templength.txt
+set /p VV=<templength.txt
+del /f templength.txt
+
+set /A b=(32768/(%VV%/1000+1))-100
+if %VV% gtr 20000 (
+	ffmpeg -i %1 -vf scale=960:540 -c:v libvpx -b:v %b%K -maxrate %b%K -bufsize 10K -c:a libvorbis -b:a 64K %1.webm
+) else (
+	ffmpeg -i %1 -c:v libvpx -b:v %b%K -maxrate %b%K -bufsize 10K -c:a libvorbis -b:a 64K %1.webm
+)
+pause
